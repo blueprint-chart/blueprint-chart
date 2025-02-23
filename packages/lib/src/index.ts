f163c2dc84acc2b732fea5baf4363b28eb2ff3e0
Pierre Romera <hello@pirhoo.com>
2025-02-23 16:30:00 +0000

    feat(lib): add public API barrel index export

---
+++ packages/lib/src/index.ts
@@ -0,0 +1,47 @@
+// Charts
+export type {
+  ChartData,
+  ChartOptions,
+  ChartRenderer,
+  ChartOptionDef,
+  ChartTypeOptions,
+  ChartTypeOptionKey,
+  LineStyle,
+  HighlightConfig,
+  AxisOptions,
+  FrameOptions,
+  AreaFillConfig,
+  AnnotationConfig,
+  LineSymbolConfig,
+  SeriesOverride,
+  Margin,
+} from './charts/types'
+
+export { createFrame } from './charts/frame/frame'
+export type { FrameElements } from './charts/frame/frame'
+
+export { createCanvas } from './charts/canvas/canvas'
+export type { CanvasElements } from './charts/canvas/canvas'
+
+export { renderVerticalAxis } from './charts/axis/vertical-axis'
+export { renderHorizontalAxis } from './charts/axis/horizontal-axis'
+
+export { renderLegend } from './charts/legend/legend'
+
+export { registerChart, getChart, getChartOptions, listCharts } from './charts/registry'
+
+export { parseData, buildChartOptions } from './charts/chart-helpers'
+export { resolvePalette, listPalettes } from './charts/palettes'
+export type { PaletteEntry } from './charts/palettes'
+
+export { resolveSeriesColor, isSeriesHidden } from './charts/series-helpers'
+export { resolveBackgroundColor, adjustColorsForBackground } from './charts/contrast'
+
+// DSL
+export { parse } from './dsl/parser'
+export { serialize } from './dsl/serializer'
+export type { AnnotationNode, AreaFillNode, ChartNode, DataNode, HighlightNode, PropertyNode, SeriesNode, StepNode } from './dsl/types'
+
+// Samples
+export { samples } from './samples'
+export type { ChartSample } from './samples'
