e57146c67bf4ea7d6d364f731bbbc8328fa3ca4b
Pierre Romera <hello@pirhoo.com>
2025-02-24 21:53:10 +0000

    feat(editor): add PreviewChart and ExportPanel components

---
+++ packages/editor/src/components/Preview/PreviewChart.vue
@@ -0,0 +1,14 @@
+<template>
+  <div
+    ref="containerRef"
+    class="w-100 h-100"
+  />
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useChartPreview } from '@/composables/useChartPreview'
+
+const containerRef = ref(null)
+useChartPreview(containerRef)
+</script>
