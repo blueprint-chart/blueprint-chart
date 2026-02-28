70acbece65fbe1ec5b872cf0b4983f179b90b026
Pierre Romera <hello@pirhoo.com>
2026-02-28 00:52:51 +0000

    feat(editor): support BPC file import on upload page

--- packages/editor/src/components/DataUpload/DataUploadPanel.vue
+++ packages/editor/src/components/DataUpload/DataUploadPanel.vue
@@ -9,6 +9,7 @@
     <DataUploadFileDrop
       class="mb-4"
       @loaded="rawInput = $event"
+      @bpc="onBpcLoaded"
     />
     <DataUploadPaste v-model="rawInput" />
   </div>
@@ -16,8 +17,20 @@
 
 <script setup lang="ts">
 import { useDataTable } from '@/composables/useDataTable'
+import { useDslSync } from '@/composables/useDslSync'
+import { useChartConfig } from '@/composables/useChartConfig'
+import { useWizard } from '@/composables/useWizard'
 import DataUploadFileDrop from './DataUploadFileDrop.vue'
 import DataUploadPaste from './DataUploadPaste.vue'
 
 const { rawInput } = useDataTable()
+const { applyDsl } = useDslSync()
+const chartConfig = useChartConfig()
+const wizard = useWizard()
+
+function onBpcLoaded(content: string) {
+  applyDsl(content)
+  rawInput.value = chartConfig.data.value
+  wizard.hydrate({ currentIndex: 2, furthestIndex: 2 })
+}
 </script>
