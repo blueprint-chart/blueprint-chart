606772a79ecb58a565e290b63d6f09ed315cb0fd
Pierre Romera <hello@pirhoo.com>
2026-03-02 11:41:00 +0100

    feat(editor): add data side panel, icon rail and floating panel

---
+++ packages/editor/src/components/Data/DataFloatingPanel.vue
@@ -0,0 +1,39 @@
+<template>
+  <PanelFloating
+    :container-ref="containerRef"
+    :title="panelTitle"
+    :position="dataFloatingPosition"
+    @dock="dockDataPanel"
+    @close="closeDataPanel"
+  >
+    <DataColumnSettings v-if="dataPanelTab === 'column'" />
+    <DataTransformPipeline v-else-if="dataPanelTab === 'transforms'" />
+    <DataParseSettings v-else-if="dataPanelTab === 'parsing'" />
+    <DataRecommendations v-else-if="dataPanelTab === 'reco'" />
+  </PanelFloating>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useEditorPanel } from '@/composables/useEditorPanel'
+import PanelFloating from '@/components/Panel/PanelFloating.vue'
+import DataColumnSettings from './DataColumnSettings.vue'
+import DataTransformPipeline from './DataTransformPipeline.vue'
+import DataParseSettings from './DataParseSettings.vue'
+import DataRecommendations from './DataRecommendations.vue'
+
+defineProps<{
+  containerRef: HTMLElement | null
+}>()
+
+const { dataPanelTab, dataFloatingPosition, dockDataPanel, closeDataPanel } = useEditorPanel()
+
+const TAB_LABELS: Record<string, string> = {
+  column: 'Column Settings',
+  transforms: 'Transforms',
+  parsing: 'Parsing',
+  reco: 'Recommendations',
+}
+
+const panelTitle = computed(() => TAB_LABELS[dataPanelTab.value] ?? 'Panel')
+</script>
