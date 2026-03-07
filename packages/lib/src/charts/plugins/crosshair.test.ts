93cecf54b5b53041f18b367bc6fd28de72f19d44
pirhoo <hello@pirhoo.com>
2026-03-07 23:15:00 +0100

    test(crosshair): add horizontal bar positioning tests

--- packages/lib/src/charts/plugins/crosshair.test.ts
+++ packages/lib/src/charts/plugins/crosshair.test.ts
@@ -55,4 +55,50 @@ describe('createCrosshairPlugin', () => {
     expect(vLine?.getAttribute('stroke')).toBe('#f00')
     expect(vLine?.getAttribute('stroke-dasharray')).toBe('2,2')
   })
+
+  it('positions crosshair at bar center for vertical bars (default)', () => {
+    const barG = document.createElementNS('http://www.w3.org/2000/svg', 'g')
+    g.appendChild(barG)
+    d3.select(barG).append('rect')
+      .attr('class', 'bc-bar')
+      .attr('x', 20).attr('y', 50)
+      .attr('width', 40).attr('height', 100)
+
+    const plugin = createCrosshairPlugin({ width: 400, height: 300 })
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    plugin.postDraw!(makeChartStub(g) as any, undefined as any)
+
+    const bar = barG.querySelector('.bc-bar') as SVGRectElement
+    bar.dispatchEvent(new MouseEvent('mouseenter'))
+
+    const vLine = g.querySelector('.bc-crosshair-v') as SVGLineElement
+    const hLine = g.querySelector('.bc-crosshair-h') as SVGLineElement
+    // Vertical crosshair at x-center of bar: 20 + 40/2 = 40
+    expect(vLine.getAttribute('x1')).toBe('40')
+    // Horizontal crosshair at top of bar
+    expect(hLine.getAttribute('y1')).toBe('50')
+  })
+
+  it('positions crosshair at bar y-center for horizontal orientation', () => {
+    const barG = document.createElementNS('http://www.w3.org/2000/svg', 'g')
+    g.appendChild(barG)
+    d3.select(barG).append('rect')
+      .attr('class', 'bc-bar')
+      .attr('x', 0).attr('y', 30)
+      .attr('width', 120).attr('height', 25)
+
+    const plugin = createCrosshairPlugin({ width: 400, height: 300, orientation: 'horizontal' })
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    plugin.postDraw!(makeChartStub(g) as any, undefined as any)
+
+    const bar = barG.querySelector('.bc-bar') as SVGRectElement
+    bar.dispatchEvent(new MouseEvent('mouseenter'))
+
+    const vLine = g.querySelector('.bc-crosshair-v') as SVGLineElement
+    const hLine = g.querySelector('.bc-crosshair-h') as SVGLineElement
+    // Vertical crosshair at right edge of bar: 0 + 120 = 120
+    expect(vLine.getAttribute('x1')).toBe('120')
+    // Horizontal crosshair at y-center of bar: 30 + 25/2 = 42.5
+    expect(hLine.getAttribute('y1')).toBe('42.5')
+  })
 })
