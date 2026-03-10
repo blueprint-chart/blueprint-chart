81523888d4f2a8562dce6640c2a1e455a75aa9d4
pirhoo <hello@pirhoo.com>
2026-03-10 08:45:36 +0000

    feat(editor): add theme section to appearance tab

---
+++ packages/editor/src/components/Editor/EditorThemeSection.vue
@@ -0,0 +1,15 @@
+<template>
+  <FormControlDropdown
+    v-model="chartTheme"
+    label="Chart Theme"
+    :options="availableThemes"
+    block
+  />
+</template>
+
+<script setup lang="ts">
+import { FormControlDropdown } from '@blueprint-chart/ui'
+import { useChartTheme } from '@/composables/useChartTheme'
+
+const { chartTheme, availableThemes } = useChartTheme()
+</script>
