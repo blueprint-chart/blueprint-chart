534093f9e497c00c78c05f53535e8589269f5046
pirhoo <hello@pirhoo.com>
2026-05-15 17:19:00 +0000

    feat(ui): add NavigationSearchPill primitive

---
+++ packages/ui/src/components/Navigation/NavigationSearchPill/NavigationSearchPill.story.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import NavigationSearchPill from './NavigationSearchPill.vue'
+</script>
+
+<template>
+  <Story title="Navigation/NavigationSearchPill">
+    <Variant title="Default (mac)">
+      <NavigationSearchPill
+        placeholder="Search…"
+        shortcut-label="⌘ K"
+        shortcut-keys="Meta+K"
+      />
+    </Variant>
+
+    <Variant title="Non-mac">
+      <NavigationSearchPill
+        placeholder="Search…"
+        shortcut-label="Ctrl K"
+        shortcut-keys="Control+K"
+      />
+    </Variant>
+
+    <Variant title="Compact">
+      <NavigationSearchPill
+        placeholder="Search…"
+        shortcut-label="⌘ K"
+        shortcut-keys="Meta+K"
+        :compact="true"
+      />
+    </Variant>
+  </Story>
+</template>
--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -84,6 +84,7 @@ export { default as NavigationToggleOption } from './components/Navigation/Navig
 export { default as NavigationIconRail } from './components/Navigation/NavigationIconRail/NavigationIconRail.vue'
 export { default as NavigationIconRailEntry } from './components/Navigation/NavigationIconRail/NavigationIconRailEntry.vue'
 export { default as NavigationLink } from './components/Navigation/NavigationLink/NavigationLink.vue'
+export { default as NavigationSearchPill } from './components/Navigation/NavigationSearchPill/NavigationSearchPill.vue'
 
 export { default as ListItemRow } from './components/List/ListItemRow/ListItemRow.vue'
 export { default as ListItemActions } from './components/List/ListItemActions/ListItemActions.vue'
