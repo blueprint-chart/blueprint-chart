aeb510ccd6e3bfb785817c6ff5f55a0ebdaa00a5
pirhoo <hello@pirhoo.com>
2026-05-16 22:29:19 +0000

    feat(ui): add NavigationWorkspaceSwitcher with initial badge

---
+++ packages/ui/src/components/Navigation/NavigationWorkspaceSwitcher/NavigationWorkspaceSwitcher.story.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import NavigationWorkspaceSwitcher from './NavigationWorkspaceSwitcher.vue'
+</script>
+
+<template>
+  <Story title="Navigation/NavigationWorkspaceSwitcher">
+    <Variant title="Default">
+      <NavigationWorkspaceSwitcher name="Blueprint" />
+    </Variant>
+    <Variant title="Longer name">
+      <NavigationWorkspaceSwitcher name="Acme Newsroom" />
+    </Variant>
+  </Story>
+</template>
