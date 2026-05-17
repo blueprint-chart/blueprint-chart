d635c042e315ee2e8e5a34cfc4ef2b5490a793b8
pirhoo <hello@pirhoo.com>
2026-05-17 20:41:44 +0000

    chore(editor): retire LandingDivider, OpenSource, Philosophy, Practices, PracticeCard

--- packages/editor/src/components/Landing/LandingDivider.test.ts
+++
@@ -1,9 +0,0 @@
-import { mount } from '@vue/test-utils'
-import LandingDivider from './LandingDivider.vue'
-
-describe('LandingDivider', () => {
-  it('renders an hr element', () => {
-    const w = mount(LandingDivider)
-    expect(w.find('hr').exists()).toBe(true)
-  })
-})
